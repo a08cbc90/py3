0d17633e9b9f052900fb1932d3d34719994825bc
a08cbc90 <a08cbc90@ospf.dip.jp>
2015-10-15 16:09:55 +0900

    新規編集

---
+++ Base/__init__.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+#
+
+r""" 基本関数一式
+end-if: A blank line
+end-for: A blank line
+end-def: 2 blank lines
+end-class: 3 blank lines
+
+http
++ util
+
+"""
+
+import json, os, re, gzip
+class Util():
+    """ Utilは他の Classes が継承しやすいように初めのほうに
+    書く必要がある。
+    """
+
+    def __init__(self):
+        pass
+
+
+    def json_valid(self, json_str=""):
+        """ JSONとして成立して無ければFalse
+        JSONとして正しいstringならTrueを返す
+        """
+
+        if not json_str:
+            """ 変数が存在しない場合 True """
+            return False
+
+        if type(json_str) is bytes:
+            try:
+                """ bytes型はstrに変更できれば通過 """
+                json_str = json_str.decode()
+            except:
+                """ 変更 できない場合False """
+                return False
+        elif type(json_str) is str:
+            """ str型はそのまま """
+            pass
+        else:
+            """ str型でない場合 True """
+            return False
+
+        try:
+            json.JSONDecoder().decode(json_str)
+        except TypeError:
+            """ 何らかの型異常 """
+            return False
+        except ValueError:
+            """ Json構文エラー """
+            return False
+
+        return True
+
+
+    def json_invalid(self, json_str=""):
+        """ JSONとして成立して無ければTrue
+        JSONとして正しいstringならFalseを返す
+        """
+        return not self.json_valid(json_str)
+
+
+    def make_dir(self, path):
+        """ path(ディレクトリ)を再帰的に作成
+        成功時: pathを返す
+        失敗時: Noneを返す
+        """
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            return None
+
+        return path
+
+
+    def dir_exist(self, path):
+        """ path がディレクトリで存在すればTrue
+        そうでなければFalse
+        """
+        return os.path.isdir(path)
+
+
+    def file_exist(self, filepath):
+        """ path がFileで存在すればTrue
+        そうでなければFalse
+        """
+        return os.path.isfile(filepath)
+
+
+    def json_read(self, filepath='/dev/null'):
+        """ filepath の中身がjsonだと仮定して取得を試み
+        読み込んだ内容を返す。
+        試験内容:
+            filepathを入れてない場合: None(正常)
+            filepathが存在しない場合: None(正常)
+            json形式ではない場合: ValueError/AttributeError: None
+        """
+        if not self.file_exist(filepath):
+            return None
+        
+        try:
+            with open(filepath) as jp:
+                j = json.load(jp)
+        except AttributeError: 
+            return None
+        except ValueError: 
+            return None
+
+        return j
+
+
+    def wf(self, filepath=None, string=None, compress=False):
+        """ WriteFile
+        String または bytes型の場合のみをファイルに書き込む。
+        そこまでのDirが用意されていなくてもDirを作るように努力する
+        試験項目:
+            filepathがNone: Noneを返す
+            stringがNone: 空ファイルを作成しようとする
+        """
+        if not filepath:
+            """ パスが存在しない場合処理しない """
+            return None
+
+        if not string:
+            """ 指定がない場合、空ファイルを作成する """
+            string = ""
+
+        """ ディレクトリ部分の検出 """
+        hit = re.match("/.*/", filepath)
+        if hit:
+            dir_path = hit.group()
+        else:
+            """ ディレクトリ部分がない場合、カレントディレクトリとして処理する """
+            dir_path = "./"
+
+        if not self.dir_exist(dir_path):
+            """ ディレクトリの作成、パーミッションエラーが発生すると止まる """
+            self.make_dir(dir_path)
+
+        if compress:
+            if type(string) is str:
+                """ 書き込み内容がStr型の場合Bin型に変更する """
+                string = string.encode()
+                string = gzip.compress(string, compresslevel=6)
+            else:
+                """ 書き込み内容がStr型でない場合は圧縮をしない """
+                pass
+
+        if type(string) is str:
+            """ Str型の場合 """
+            with open(filepath, "w") as fp:
+                fp.write(string)
+        elif type(string) is bytes:
+            """ バイナリの場合 """
+            with open(filepath, "wb") as fp:
+                fp.write(string)
+        else:
+            """ それ以外の型の場合何もしない """
+            return None
+
+        """ 正常に終了した場合はその文字を返す """
+        return string
+
+
+    def af(self, filepath=None, string=None):
+        """ AddFile
+        String または bytes型の場合のみをファイルに追記する。
+        そこまでのDirが用意されていなくてもDirを作るように努力する
+        圧縮オプションは搭載しない
+        正常終了時は、追記した部分のみStringを返す
+        """
+        if not filepath:
+            """ パスが存在しない場合処理しない """
+            return None
+
+        if not string:
+            """ 指定がない場合、空ファイルを作成する """
+            string = ""
+
+        """ ディレクトリ部分の検出 """
+        hit = re.match("/.*/", filepath)
+        if hit:
+            dir_path = hit.group()
+        else:
+            """ ディレクトリ部分がない場合、カレントディレクトリとして処理する """
+            dir_path = "./"
+
+        if not self.dir_exist(dir_path):
+            """ ディレクトリの作成、パーミッションエラーが発生すると止まる """
+            self.make_dir(dir_path)
+
+        if type(string) is str:
+            """ Str型の場合 """
+            with open(filepath, "a") as fp:
+                fp.write(string)
+        elif type(string) is bytes:
+            """ バイナリの場合 """
+            with open(filepath, "ab") as fp:
+                fp.write(string)
+        else:
+            """ それ以外の型の場合何もしない """
+            return None
+
+        """ 正常に終了した場合はその文字を返す """
+        return string
+
+
+    def rf(self, filepath=None, compress=False):
+        """ ReadFile
+        ファイルをStringとして読み込む
+        2015/09/23 zip対応
+        """
+        if not self.file_exist(filepath):
+            """ 存在しない場合は None を返す """
+            return None
+
+        if compress:
+            with open(filepath, "rb") as fp:
+                data = fp.read()
+                data = gzip.decompress(data).decode()
+        else:
+            with open(filepath) as fp:
+                data = fp.read()
+
+        return data
+
+    def rm(self, filepath=None):
+        """ REmove
+        ファイルを削除する。消えたかどうかは判定しない
+        """
+        try:
+            os.remove(filepath)
+        except:
+            return None
+
+        return None
+
+
+    def file_timestamp_delay(self, path=None):
+        """ ファイルのModifyタイムスタンプが現在から何秒ずれているかを整数で返す
+        ファイルが無い場合は 1900年前との差を返す。
+        未来のタイムスタンプであれば 数値は負の整数に成る。
+        """
+        """ t = 現在時刻[sec] """
+        t = datetime.datetime.now().strftime('%s')
+        if not self.file_exist(path):
+            """ ファイルが存在しない場合 """
+            return int(t)
+
+        return int(os.stat(path).st_mtime - int(t))
+
+import http.cookiejar, urllib, gzip, datetime, time, socket
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ start.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.4
+# -*- coding: utf-8 -*-
+#
+
+r"""testしたりimportするだけ～
+"""
+
+if __name__ != '__main__':
+    print("Do as main.(exit)")
+    exit()
+
+import Base
+B = Base.Util()
