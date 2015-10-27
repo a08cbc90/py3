22477acd50d077ba27b5ea2bfdf55760db8e8aa3
a08cbc90 <a08cbc90@ospf.dip.jp>
2015-10-27 13:31:05 +0900

    少し整理

--- k2init.py
+++ k2init.py
@@ -13,18 +13,22 @@ import K2
 C = K2.Gen()
 print("JsonLoaded?: " + C.ht_url)
 g = C.get_accessible_symbols()
-print(C.DP)
+#print(C.DP)
 exit()
-"""
+""" 確認済み関数
+print(C.klogin())
+print(C.ht_counter_print())
 g = C.get_accessible_symbols()
 h = C.download_k2_summary(Z)
 p = C.summary_picker(h)
 print (C.deq_picker(i, p))
 print(p)
+print(C.s_sec_collection())
+print(C.s_mst_collection())
+print(C.s_elk_collection())
+C.add_crts_gains()
 """
 exit()
-print(C.klogin())
-print(C.ht_counter_print())
 
 
 
