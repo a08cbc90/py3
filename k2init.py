6d72e18928ee924b293e3aa9248e0410684a5d24
a08cbc90 <a08cbc90@ospf.dip.jp>
2015-10-28 18:18:36 +0900

    add_crts_gains追加動作盛り込み

--- k2init.py
+++ k2init.py
@@ -13,10 +13,11 @@ import K2
 C = K2.Gen()
 print("JsonLoaded?: " + C.ht_url)
 g = C.get_accessible_symbols()
+C.add_crts_gains()
 #print(C.DP)
 exit()
 """ 確認済み関数
-print(C.klogin())
+C.klogin()
 print(C.ht_counter_print())
 g = C.get_accessible_symbols()
 h = C.download_k2_summary(Z)
@@ -26,7 +27,6 @@ print(p)
 print(C.s_sec_collection())
 print(C.s_mst_collection())
 print(C.s_elk_collection())
-C.add_crts_gains()
 """
 exit()
 
